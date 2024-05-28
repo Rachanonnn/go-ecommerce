508a16a3a89418f7c8d66fc034ae8c9cc60588cd
watchakorn-18k <wk18k@proton.me>
2024-05-29 04:17:07 +0700

    fix : change image example url

--- Frontend/src/Components/ProductCard.tsx
+++ Frontend/src/Components/ProductCard.tsx
@@ -11,13 +11,13 @@ const ProductCard: React.FC<Props> = ({ name, price }) => {
       <div className="card card-compact w-96 bg-base-100 shadow-xl hover:scale-[1.02] duration-300">
         <figure>
           <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            src="https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg"
             alt="Shoes"
           />
         </figure>
         <div className="card-body">
           <h2 className="card-title">{name}</h2>
-          <p>Price: {price}</p>
+          <p>Price: {price} Baht</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Buy Now</button>
           </div>
