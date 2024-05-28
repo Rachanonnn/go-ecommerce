b3b6bcbafaf564cd76d1e5d65a7ff1bd22afa8a4
watchakorn-18k <wk18k@proton.me>
2024-05-29 05:05:27 +0700

    fix : env firebase apikey config

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -2,7 +2,13 @@
 const nextConfig = {
     env: {
         BACKEND_URL: process.env.BACKEND_URL, // pulls from .env file
-        FIREBASE_CONFIG: process.env.FIREBASE_CONFIG,
+        FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+        FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+        FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+        FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+        FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+        FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
     },
 };
 
