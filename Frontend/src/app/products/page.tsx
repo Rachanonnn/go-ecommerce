4b91a6734c7dba7fc95aeecfed7bcefdfe4e9a67
watchakorn-18k <wk18k@proton.me>
2024-05-30 01:44:44 +0700

    feat : add product show skeleton before fetch data

--- Frontend/src/app/products/page.tsx
+++ Frontend/src/app/products/page.tsx
@@ -4,6 +4,7 @@ import getAllProducts from "@/libs/product/getProduct";
 import React from "react";
 import ProductCard from "@/Components/ProductCard";
 import ModalAddProduct from "@/Components/ModalAddProduct";
+import ProductCardSkeleton from "@/Components/ProductCardSkeleton";
 
 // Define the Product interface
 interface Product {
@@ -13,13 +14,17 @@ interface Product {
   price: number;
 }
 
-const page = () => {
+const Page = () => {
   // Use the Product interface to type the state
   const [productData, setProductData] = React.useState<Product[]>([]);
+  const [loading, setLoading] = React.useState<boolean>(true);
+
   const fetchProducts = React.useCallback(() => {
+    setLoading(true);
     getAllProducts().then((response) => {
       const { data } = response;
       setProductData(data);
+      setLoading(false);
     });
   }, []);
 
@@ -41,15 +46,25 @@ const page = () => {
               (document.getElementById('modalAddProduct') as HTMLFormElement).showModal();
             }
           }}
-        >Add Product</button>
+        >
+          Add Product
+        </button>
       </div>
       <div className="flex flex-wrap gap-5 justify-center items-center mx-auto mt-10">
-        {productData.map((product, index) => (
-          <ProductCard key={index} name={product.product_name} price={product.price} />
-        ))}
+        {loading
+          ? Array.from({ length: 8 }).map((_, index) => (
+            <ProductCardSkeleton key={index} />
+          ))
+          : productData.map((product) => (
+            <ProductCard
+              key={product.product_id}
+              name={product.product_name}
+              price={product.price}
+            />
+          ))}
       </div>
     </>
   );
 };
 
-export default page;
+export default Page;
