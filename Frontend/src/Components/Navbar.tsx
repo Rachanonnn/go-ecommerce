b99d45430488f01eb00a90633d48dc9f77084a93
Rachanonnn <rachanonnn2004@gmail.com>
2024-05-31 01:00:35 +0700

    fix: total items, total price

--- Frontend/src/Components/Navbar.tsx
+++ Frontend/src/Components/Navbar.tsx
@@ -1,12 +1,37 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useUserAuth } from "@/libs/context/UserAuthContext";
 import checkCookies from "@/libs/user/checkCookies";
+import getitemfromcart from "@/libs/user/getitemfromcart";
 
 const Navbar = () => {
   const router = useRouter();
+  const { user } = useUserAuth();
+  const [cartItems, setCartItems] = useState(0);
+  const [totalCartPrice, setTotalCartPrice] = React.useState<number>(0);
+
+  useEffect(() => {
+    const fetchCartItems = async () => {
+      try {
+        if (!user) return; // Check if user exists
+        let totalCartPrice = 0;
+        const response = await getitemfromcart(user.uid);
+        const cartItemCount = response.data.cart_data.length;
+        for (let i = 0; i < cartItemCount; i++) {
+          const price = response.data.cart_data[i].total_price;
+          totalCartPrice += price;
+        }
+        setCartItems(cartItemCount);
+        setTotalCartPrice(totalCartPrice);
+      } catch (error) {
+        console.error("Error fetching order data:", error);
+      }
+    };
+    fetchCartItems();
+  }, [user]);
+
   const { logOut } = useUserAuth();
   // checkCookies();
   const [isToken, setIsToken] = React.useState(false);
@@ -64,8 +89,8 @@ const Navbar = () => {
               className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
             >
               <div className="card-body">
-                <span className="font-bold text-lg">0 Items</span>
-                <span className="text-info">Subtotal: $0</span>
+                <span className="font-bold text-lg">{cartItems} Items</span>
+                <span className="text-info">Total: ${totalCartPrice}</span>
                 <div className="card-actions">
                   <button
                     onClick={() => router.push("/cart")}
