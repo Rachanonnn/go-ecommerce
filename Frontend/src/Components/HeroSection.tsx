a6df6ec6d5bdae89901d5929b39bfd277c1c7956
Rachanonnn <rachanonnn2004@gmail.com>
2024-07-12 17:15:01 +0700

    fix: apos

--- Frontend/src/Components/HeroSection.tsx
+++ Frontend/src/Components/HeroSection.tsx
@@ -16,10 +16,10 @@ const HeroSection = () => {
           <div className="max-w-md">
             <h1 className="text-5xl font-bold">Hello there</h1>
             <p className="py-6">
-              Welcome to our e-commerce project, where we've merged the power of
-              Go Fiber for the backend with React and Next.js for the frontend.
-              Explore our site to discover a curated selection of products and
-              enjoy hassle-free shopping at your fingertips!
+              Welcome to our e-commerce project, where we&apos;ve merged the
+              power of Go Fiber for the backend with React and Next.js for the
+              frontend. Explore our site to discover a curated selection of
+              products and enjoy hassle-free shopping at your fingertips!
             </p>
             <a className="btn btn-primary" href="/website/home">
               Get Started
