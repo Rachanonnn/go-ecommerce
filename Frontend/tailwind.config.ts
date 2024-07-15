7476c0e67ea6c85b16f209cf609208fbf1cd06db
Rachanonnn <rachanonnn2004@gmail.com>
2024-07-15 12:13:12 +0700

    fix: tailwind config

--- Frontend/tailwind.config.ts
+++ Frontend/tailwind.config.ts
@@ -10,6 +10,8 @@ const config: Config = {
     "./src/app/website/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{html,js}",
     "./src/app/website/**/*.{html,js}",
+    "./src/**/*.{html,js}",
+    "./src/website/**/*.{html,js}",
   ],
   theme: {
     extend: {
